f8d39416ac1afc8f51daaafbea774b19b5657e40
Natalia Stanko <anithaly@gmail.com>
2019-10-14 16:49:12 +0200

    add gestures and sound effects

---
+++ speech.py
@@ -0,0 +1,23 @@
+import speech
+from microbit import sleep
+
+# Singing requires a phoneme with an annotated pitch for each syllable.
+solfa = [
+    "#115DOWWWWWW",   # Doh
+    "#103REYYYYYY",   # Re
+    "#94MIYYYYYY",    # Mi
+    "#88FAOAOAOAOR",  # Fa
+    "#78SOHWWWWW",    # Soh
+    "#70LAOAOAOAOR",  # La
+    "#62TIYYYYYY",    # Ti
+    "#58DOWWWWWW",    # Doh
+]
+
+# Sing the scale ascending in pitch.
+song = ''.join(solfa)
+speech.sing(song, speed=100)
+# Reverse the list of syllables.
+solfa.reverse()
+song = ''.join(solfa)
+# Sing the scale descending in pitch.
+speech.sing(song, speed=100)
