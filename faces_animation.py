c9dd2e7318ffaff490e9268667f1170530387dd5
Natalia Stanko <anithaly@gmail.com>
2019-10-14 16:36:34 +0200

    play with leds

---
+++ faces_animation.py
@@ -0,0 +1,3 @@
+from microbit import*
+faces = [Image.HAPPY, Image.SAD]
+display.show(faces, loop=True, delay=900)
---
+++ infinity_image.py
@@ -0,0 +1,9 @@
+from microbit import*
+infinity = Image(
+            "99000:"
+            "90900:"
+            "09990:"
+            "00909:"
+            "00099")
+
+display.show(infinity)
